d7acf345d15e900450cdd66dc8a4fc2ad287d89b
Benjamin Hargett <hargettbenjamin@gmail.com>
2019-08-30 10:31:15 -0400

    add font_family and font_weight

--- lib/apple-news/styles/text.rb
+++ lib/apple-news/styles/text.rb
@@ -1,7 +1,7 @@
 module AppleNewsClient
   module Style
     class Text < Base
-      optional_properties :background_color, :font_name, :font_size, :text_color, :text_shadow,
+      optional_properties :background_color, :font_name, :font_family, :font_weight, :font_size, :text_color, :text_shadow,
                           :strikethrough, :stroke, :tracking, :underline, :vertical_alignment
     end
   end
